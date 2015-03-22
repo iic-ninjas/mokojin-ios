11fec865ae36502a8475743d24b31d3e576dd970
yon <Lighthawky@gmail.com>
2015-03-22 18:07:05 +0200

    basics

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'mokojin' do
+  pod 'Parse', '~> 1.6'
+end
+
+target 'mokojinTests' do
+
+end
--- mokojin/AppDelegate.swift
+++ mokojin/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("GeJyJhvvsIe540zKyn9rCZwSv7AIEcc11DHQjSAV", clientKey: "40quo2Icf83unfXkDu2ZJjEcecPsHl03aqiuNsbH")
+        PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(nil, block: nil)
         return true
     }
 
